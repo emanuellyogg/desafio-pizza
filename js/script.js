12b425728911b9afdc381579681fbefce599694d
Emanuelly Maria Ogg Vicente <emanuelly.vicente@capgemini.com>
2021-08-13 16:34:07 -0300

    feat: criado arquivo script

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//TODO capturar os dados inseridos na tela
+
+var btIncluirPizza = document.querySelector("#")
+
+btIncluirPizza.addEventListener("click", function(event){
+  event.preventDefault();
+})
+
+// TODO armazenar esses dados em array
+
+//  TODO exibir os dados capturados
+
+// TODO calcular o valor por cm²
+
+// TODO ordenar a lista do mais barato ao mais caro
+
+// TODO calcular o percentual de diferença do preço de um tamanho para o outro. 
